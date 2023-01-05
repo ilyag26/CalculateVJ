94d855b1a4a40c55f4fa0e59c59b0844564eacdb
«Ilya <ytgaivi@gmail.com>
2023-01-05 11:25:40 +0100

    Code optimization

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,28 +1,27 @@
 public class Calculate {
-    public static void printResultPlus(double num1, double num2, double sum, String str){
+    public static void printMethod(double num1, double num2, double sum, String str){
+        String result = sum + " y esto "+ (sum < 0 ? "negativo" : "positivo") + " Número";
+        System.out.print(num1 + str + num2 +" " +"= " + result);
+    }
+    public static void printResultPlus(double num1, double num2, double sum, String string){
         sum = num1 +num2;
-        System.out.print(num1 + str + num2 +" " +"= ");
-        System.out.print(sum + " y esto "+ (sum < 0 ? "negativo" : "positivo") + " Número");
+        printMethod(num1,num2,sum,string);
     }
     public static void printResultMinus(double num1,double num2, double sum, String string){
         sum = num1 - num2;
-        System.out.print(num1 + string + num2 +" " +"= ");
-        System.out.print(sum + " y esto "+ (sum < 0 ? "negativo" : "positivo") + " Número");
+        printMethod(num1,num2,sum,string);
     }
     public static void printResultDivision(double num1,double num2, double sum, String string){
         sum = num1 - num2;
-        System.out.print(num1 + string + num2 +" " +"= ");
-        System.out.print(sum + " y esto "+ (sum < 0 ? "negativo" : "positivo") + " Número");
+        printMethod(num1,num2,sum,string);
     }
     public static void printResultMultiplication(double num1,double num2, double sum, String string){
         sum = num1 - num2;
-        System.out.print(num1 + string + num2 +" " +"= ");
-        System.out.print(sum + " y esto "+ (sum < 0 ? "negativo" : "positivo") + " Número");
+        printMethod(num1,num2,sum,string);
     }
     public static void printResultReturn(double num1,double num2, double sum, String string){
         sum = num1 % num2;
-        System.out.print(num1 + string + num2 +" " +"= ");
-        System.out.print(sum + " y esto "+ (sum < 0 ? "negativo" : "positivo") + " Número");
+        printMethod(num1,num2,sum,string);
     }
     public static void compareLogic(double num1,double num2,double result,String s1){
         int count = 0;
